1b00a99d0fefe44daddc5314ff68efb8af613556
chaotang <allent@wicresoft.com>
2019-06-18 16:43:24 +0800

    Update BMW demo

--- run_all_cases.py
+++ run_all_cases.py
@@ -2,15 +2,13 @@
 
 import unittest
 import time
-import os, sys
-sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+import os
 from BeautifulReport.BeautifulReport import BeautifulReport
 from common import send_email
 
 
 base_dir = os.path.dirname(os.path.realpath(__file__))
 test_cases_path = os.path.join(base_dir, 'test_case')
-
 
 
 def create_suite():
